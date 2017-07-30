82539c6ec94616049e359eaafbf7bda1e31140e6
webmasterdragon <webmasterdragon@outlook.com>
2017-07-30 10:44:14 +0700

    angular_gallery

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'ng-root',
+  selector: 'ng-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
