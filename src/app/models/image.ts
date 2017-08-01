7f9e3c0a451d26125b6ba22c1cbc31e8c794e268
webmasterdragon <webmasterdragon@outlook.com>
2017-08-01 23:21:23 +0700

    model imagedetails

--- src/app/models/image.ts
+++ src/app/models/image.ts
@@ -1,4 +1,4 @@
 export class Image {
-  constructor(public id:string, public title:string, public description:string, public thumbnail:string, public imageLink:string) {
+  constructor(public id: string, public title: string, public description: string, public thumbnail: string, public imageLink: string) {
   }
 }
